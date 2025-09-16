fe357ddc8444ab3f0de689695beb57f07183270f
Bruna Duarte Ferreira <bruna.rj1@outlook.com.br>
2025-09-16 18:54:25 -0300

    Adiciona exercício 1: nome e idade

--- fundamentos-python/1_variaveis_operadores/exercicio1_nome_idade.py
+++ fundamentos-python/1_variaveis_operadores/exercicio1_nome_idade.py
@@ -1,6 +1,4 @@
 # Exercício 1: Solicitar nome e idade e exibir uma mensagem personalizada
-
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-
-print(f"Olá, {nome}! Você tem {idade} anos.")
+nome = input('Olá, por favor digite o seu nome: ')
+idade = input ('Agora a sua idade: ')
+print('Seu nome é {} e você tem {} anos.'.format(nome, idade))
